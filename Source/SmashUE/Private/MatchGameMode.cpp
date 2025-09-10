1d48aa2f1aba21e6cf3805c740f340a06dc2cecb
Perka <lemergabriel55@gmail.com>
2025-09-10 15:55:04 +0200

    [ADD] MatchGameMode, ASmashCharacter, SpawnArenaFinder

---
+++ Source/SmashUE/Private/MatchGameMode.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MatchGameMode.h"
+
+#include "Arena/ArenaPlayerStart.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void AMatchGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	TArray<AArenaPlayerStart*> spawnPoints;
+	GetAllSpawnPoints(spawnPoints);
+
+
+
+	for (auto sp : spawnPoints)
+	{
+		if (sp != nullptr)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, sp->GetName());
+		}
+	}
+}
+
+void AMatchGameMode::GetAllSpawnPoints(TArray<AArenaPlayerStart*>& FoundActors)
+{
+	TArray<AActor*> SpawnPoints;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AArenaPlayerStart::StaticClass(), SpawnPoints);
+	for (AActor* SpawnPoint : SpawnPoints)
+	{
+		AArenaPlayerStart* spawnPlayerStart = Cast<AArenaPlayerStart>(SpawnPoint);
+		if (spawnPlayerStart == nullptr){continue;}
+		FoundActors.Add(spawnPlayerStart);
+	}
+}
---
+++ Source/SmashUE/Public/MatchGameMode.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MatchGameMode.generated.h"
+
+class AArenaPlayerStart; // WHY NOT #INCLUDE ?
+
+/**
+ * 
+ */
+
+UCLASS()
+class SMASHUE_API AMatchGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+private:
+	void GetAllSpawnPoints(TArray<AArenaPlayerStart*>& FoundActors);
+};
